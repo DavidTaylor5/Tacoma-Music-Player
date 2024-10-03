e398ca5456a43bbbc08e3e2f58001c6ea0cdf2b1
David Taylor <david.a.taylor253@gmail.com>
2024-10-02 21:35:39 -0700

    Change library animation to be black background

--- app/src/main/java/com/example/tacomamusicplayer/fragment/MusicPlayingFragment.kt
+++ app/src/main/java/com/example/tacomamusicplayer/fragment/MusicPlayingFragment.kt
@@ -1,28 +1,21 @@
 package com.example.tacomamusicplayer.fragment
 
 import android.graphics.drawable.AnimationDrawable
-import android.net.Uri
 import android.os.Bundle
 import android.view.GestureDetector
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
 import androidx.annotation.OptIn
-import androidx.core.view.GestureDetectorCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.session.MediaController
 import androidx.navigation.fragment.findNavController
-import androidx.viewpager2.widget.ViewPager2
 import com.example.tacomamusicplayer.R
-import com.example.tacomamusicplayer.adapter.ScreenSlidePagerAdapter
 import com.example.tacomamusicplayer.databinding.FragmentMusicPlayingBinding
 import com.example.tacomamusicplayer.enum.ScreenType
-import com.example.tacomamusicplayer.util.MusicGestureDetector
 import com.example.tacomamusicplayer.viewmodel.MainViewModel
 import timber.log.Timber
 
@@ -110,7 +103,7 @@ class MusicPlayingFragment: Fragment() {
     }
 
     private fun setupLibraryButtonAnimation(binding:FragmentMusicPlayingBinding) {
-        binding.libraryButton!!.setBackgroundResource(R.drawable.library_button_animation)
+        binding.libraryButton!!.setBackgroundResource(R.drawable.library_animation)
         val frameAnimation = binding.libraryButton.background as AnimationDrawable
         frameAnimation.start()
     }
