70b0e40fbcffef9ffc465b22db626e483a8cf599
davidtaylor5 <davidtaylor@tecace.com>
2025-03-08 14:48:01 -0800

    Added all animations, and create initial CustomSettingsPrompt and CusotmSortingPrompt.

---
+++ app/src/main/java/com/andaagii/tacomamusicplayer/view/CustomSettingsPrompt.kt
@@ -0,0 +1,24 @@
+package com.andaagii.tacomamusicplayer.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.andaagii.tacomamusicplayer.databinding.ViewCustomSettingsPromptBinding
+
+class CustomSettingsPrompt @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): LinearLayout(context, attrs, defStyleAttr) {
+
+    private lateinit var binding: ViewCustomSettingsPromptBinding
+
+    init {
+        attrs?.let {
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            binding = ViewCustomSettingsPromptBinding.inflate(inflater, this, true)
+        }
+    }
+}
