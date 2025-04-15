6d6db19a0da9b61ff3e01924264904002c4b1b7e
David Taylor <davidtaylor@tecace.com>
2025-04-15 12:04:23 -0700

    Fix for search UI showing up when I swipe away from search and press an album. Fix for showing search results minus 1.

--- app/src/main/java/com/andaagii/tacomamusicplayer/fragment/pages/SongListFragment.kt
+++ app/src/main/java/com/andaagii/tacomamusicplayer/fragment/pages/SongListFragment.kt
@@ -148,6 +148,9 @@ class SongListFragment(
 
             currentSongGroup = songGroup
 
+            //new currentSongList means that the search is over, remove search stuff
+           parentViewModel.handleCancelSearchButtonClick()
+
             binding.displayRecyclerview.adapter = SongListAdapter(
                 songGroup.songs,
                 this::handleSongSetting,
@@ -192,7 +195,7 @@ class SongListFragment(
             } else if(searchItems.size > 10) {
                 searchItems.subList(0, 10)
             } else {
-                searchItems.subList(0, searchItems.size -1)
+                searchItems.subList(0, searchItems.size)
             }
 
             val topTenSongs =  MediaItemUtil().convertListOfSearchDataIntoListOfMediaItem(topSearchData)
