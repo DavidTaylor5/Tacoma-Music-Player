4038ae1bf9ebfc1f560bcc8accd3dbb34f64aff5
David Taylor <davidtaylor@tecace.com>
2025-04-24 17:33:45 -0700

    Change from top 10 results to top 20 so that an entire album can be shown.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,26 +39,27 @@ android {
         }
     }
 
-    signingConfigs {
-        create("release") {
-            storeFile = file(project.property("RELEASE_STORE_FILE") as String)
-            storePassword = project.property("RELEASE_STORE_PASSWORD") as String
-            keyAlias = project.property("RELEASE_KEY_ALIAS") as String
-            keyPassword = project.property("RELEASE_KEY_PASSWORD") as String
-        }
-    }
+//    signingConfigs {
+//        create("release") {
+//            storeFile = file(project.property("RELEASE_STORE_FILE") as String)
+//            storePassword = project.property("RELEASE_STORE_PASSWORD") as String
+//            keyAlias = project.property("RELEASE_KEY_ALIAS") as String
+//            keyPassword = project.property("RELEASE_KEY_PASSWORD") as String
+//        }
+//    }
 
     buildTypes {
         debug {
 
         }
         release {
-            signingConfig = signingConfigs.getByName("release")
+//            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
