ce19459a0190624b374b1d604e04c24bfa83091f
David Taylor <davidtaylor@tecace.com>
2025-05-22 14:18:34 -0700

    Fix for switching albums and changing shuffle mode causing previous ordered album to show up.

--- app/src/main/java/com/andaagii/tacomamusicplayer/TacomaMusicPlayerApplication.kt
+++ app/src/main/java/com/andaagii/tacomamusicplayer/TacomaMusicPlayerApplication.kt
@@ -3,7 +3,6 @@ package com.andaagii.tacomamusicplayer
 import android.app.Application
 import com.andaagii.tacomamusicplayer.util.FileLoggingTree
 import timber.log.Timber
-import java.io.File
 
 class TacomaMusicPlayerApplication: Application() {
 
@@ -11,7 +10,6 @@ class TacomaMusicPlayerApplication: Application() {
         super.onCreate()
 
         if(BuildConfig.DEBUG) {
-//            Timber.plant(Timber.DebugTree())
             Timber.plant(FileLoggingTree(
                 logDir = getExternalFilesDir(null),
             ))
