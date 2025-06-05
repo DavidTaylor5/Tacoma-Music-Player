93fd2b30b4e1f0d5d74c9d4b75ac3ec23ebe9c04
David Taylor <david.a.taylor253@gmail.com>
2025-06-05 13:20:26 -0700

    Set isMinifyEnabled to true

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
         }
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
