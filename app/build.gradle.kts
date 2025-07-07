98b3d8d54a06b4a00e3c53a5901f8579e1ab6fe6
David Taylor <david.a.taylor253@gmail.com>
2025-07-07 00:07:41 -0700

    Update to version 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.andaagii.tacomamusicplayer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
