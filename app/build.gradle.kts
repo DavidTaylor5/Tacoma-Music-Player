301858c8c7b486648515289b62a0ce2374e9d41b
David Taylor <david.a.taylor253@gmail.com>
2026-01-16 22:48:46 -0800

    version 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.andaagii.tacomamusicplayer"
         minSdk = 30
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.8"
+        versionCode = 9
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
